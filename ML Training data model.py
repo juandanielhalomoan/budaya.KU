53ffad31a6ad059e35e162751f9ab528d695034e
carloanthonyyy <79904176+carloanthonyyy@users.noreply.github.com>
2021-06-09 13:58:03 +0700

    Update ML Training data model.py

--- ML Training data model.py
+++ ML Training data model.py
@@ -4,15 +4,19 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 import numpy as np
-
 import glob
+
+#download dan unzip dataset buatan dari google drive
 from google_drive_downloader import GoogleDriveDownloader as gdd
 gdd.download_file_from_google_drive(file_id='18O90Nag0DGs4eVHyceVKiXVmVsjkRD6Q',
                                     dest_path='/content/Budayaku_dataset.zip',
                                     unzip=True)
+
+#Jumlah dataset
 image_count = len(list(glob.glob('/content/Budayaku_dataset/*/*.jpg')))
 print(image_count)
 
+#pembagian data training dan data valdation
 batch_size = 32
 img_height = 180
 img_width = 180
@@ -31,35 +35,32 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
+#nama setiap kelas klasifikasi
 class_names = train_ds.class_names
 print(class_names)
 
+#penampilan data gambar dan kelas klasifikasi
 plt.figure(figsize=(10, 10))
 for images, labels in train_ds.take(1):
   for i in range(9):
     ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.imshow(images[i].numpy().astype(np.uint8))
     plt.title(class_names[labels[i]])
     plt.axis("off")
-
 for image_batch, labels_batch in train_ds:
   print(image_batch.shape)
   print(labels_batch.shape)
   break
 
+#Penyimpanan gambar dalam memori dan menghindarkan tumpang tindih saat pelaksanaan training
 AUTOTUNE = tf.data.AUTOTUNE
-
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
+#Pembuatan Model
 num_classes = 13
-
 model = Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.experimental.preprocessing.Rescaling(1, input_shape=(img_height, img_width, 3)),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
@@ -76,12 +77,15 @@ model.compile(optimizer='adam',
 
 model.summary()
 
+#Training model
 epochs=10
 history = model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=epochs
 )
+
+#Menampilkan grafik hasil training
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
@@ -104,7 +108,27 @@ plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
 
+#Konversi model ke dalam TFLite
 from tensorflow import lite
+def representative_data_gen():
+  dataset_list = tf.data.Dataset.list_files('/content/Budayaku_dataset/*/*.jpg')
+  for i in range(100):
+    image = next(iter(dataset_list))
+    image = tf.io.read_file(image)
+    image = tf.io.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [img_height, img_width])
+    image = tf.cast(image / 255., tf.float32)
+    image = tf.expand_dims(image, 0)
+    yield [image]
+    
 converter = lite.TFLiteConverter.from_keras_model(model)
-tfmodel = converter.convert()
-open('budayaku.tflite', 'wb').write(tfmodel)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_data_gen
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.target_spec.supported_types = [tf.int8]
+converter.inference_input_type = tf.uint8
+converter.inference_output_type = tf.uint8
+
+tflite_model = converter.convert()
+with open('budayaku.tflite', 'wb') as f:
+  f.write(tflite_model)
